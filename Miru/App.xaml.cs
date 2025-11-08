10e1657e8898785995de5b7718d209c6ddf0a50d
Iyarashii <strider1337v@gmail.com>
2025-11-08 23:44:32 +0100

    Update github username

--- Miru/App.xaml.cs
+++ Miru/App.xaml.cs
@@ -1,6 +1,6 @@
-﻿// Copyright (c) 2022 iyarashii @ https://github.com/iyarashii 
+﻿// Copyright (c) 2022 iyashi39 @ https://github.com/iyashi39 
 // Licensed under the GNU General Public License v3.0,
-// go to https://github.com/iyarashii/Miru/blob/master/LICENSE for full license details.
+// go to https://github.com/iyashi39/Miru/blob/master/LICENSE for full license details.
 
 using System.Windows;
 
